4ad1abc07c90c08f1b80f6fef21604e4f335d168
jddolvin <justindolvin@gmail.com>
2021-11-02 02:21:31 -0400

    created boilerplate layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+import Radio from "./Radio";
+import About from "./About";
+import Contact from "./Contact";
+import Games from "./Games";
+import Home from "./Home";
+import Programming from "./Programming";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/" class="nav">Home</Link>
+        <Link to="/programming" class="nav">Programming Projects</Link>
+        <Link to="/radio" class="nav">Radio Projects</Link>
+        <Link to="/games" class="nav">Games</Link>
+        <Link to="/about" class="nav">About Me</Link>
+        <Link to="/contact" class="nav">Contact Me</Link>
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route exact={true} path="/programming">
+            <Programming />
+          </Route>
+          <Route exact={true} path="/radio">
+            <Radio />
+          </Route>
+          <Route exact={true} path="/games">
+            <Games />
+          </Route>
+          <Route exact={true} path="/about">
+            <About />
+          </Route>
+          <Route exact={true} path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Programming.js
@@ -0,0 +1,3 @@
+export default function Programming() {
+    return <div>lorem ipsum</div>
+}
