2637b25aeadda312418bc0aaf96db7242ffcc45b
jddolvin <justindolvin@gmail.com>
2021-12-01 18:21:51 -0500

    boilerplate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <>
+      <div className="center noPokemon">
+        Boilerplate homepage content, nothing meaningful here yet, feel free to check back later
+      </div>
       <footer id="footer">© Justin Dolvin 2021</footer>
     </>
   );
--- src/pages/Programming.js
+++ src/pages/Programming.js
@@ -1,6 +1,10 @@
 export default function Programming() {
   return (
     <>
+      <div className="center noPokemon">
+        Boilerplate project content, nothing meaningful here yet, feel free to
+        check back later
+      </div>
       <footer id="footer">© Justin Dolvin 2021</footer>
     </>
   );
