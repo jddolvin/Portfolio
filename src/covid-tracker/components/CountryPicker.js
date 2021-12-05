ec70c5e8ce39a599f3924b5a1d72c855a43314f7
jddolvin <justindolvin@gmail.com>
2021-12-05 18:20:20 -0500

    improved formatting

--- src/covid-tracker/components/CountryPicker.js
+++ src/covid-tracker/components/CountryPicker.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { NativeSelect, FormControl } from "@material-ui/core";
 import styles from "../../stylesheets/CountryPicker.module.css";
 import { fetchCountries } from "../api";
+import { makeStyles } from "@material-ui/core/styles";
 
 const CountryPicker = ({ handleCountryChange }) => {
   const [fetchedCountries, setFetchedCountries] = useState([]);
@@ -11,11 +12,18 @@ const CountryPicker = ({ handleCountryChange }) => {
     };
     fetchAPI();
   }, [setFetchedCountries]);
-
+  const useStyles = makeStyles({
+    input: {
+      color: "#5bc0de",
+    },
+  });
+  const classes = useStyles();
   return (
     <FormControl className={styles.FormControl}>
       <NativeSelect
+        inputProps={{className: classes.input}}
         defaultValue=""
+        labelStyle={{ color: "skyblue" }}
         onChange={(e) => handleCountryChange(e.target.value)}
       >
         <option value="">Global</option>
